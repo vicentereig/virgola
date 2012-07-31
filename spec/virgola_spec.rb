5f77891b27b765ee01563ebe3a54245be0db465f
Vicente Reig <vicente.reig@gmail.com>
2012-07-31 10:51:13 +0200

    Working In Memory with HasMany/HasOne

--- spec/virgola_spec.rb
+++ spec/virgola_spec.rb
@@ -6,8 +6,15 @@ class CompanyProfile
   column :email
   column :phone
 
+  belongs_to :developer
+
   def initialize
     yield self if block_given?
+  end
+
+  def ==(profile)
+    return false unless profile.is_a?(self.class)
+    self.email == profile.email && self.phone == profile.phone
   end
 end
 
@@ -20,6 +27,11 @@ class Task
 
   def initialize
     yield self if block_given?
+  end
+
+  def ==(task)
+    return false unless task.is_a?(self.class)
+    self.title == task.title && self.description == task.description
   end
 end
 
@@ -38,18 +50,19 @@ class Developer
   end
 
   def ==(pip)
-    return false unless pip.is_a?(Developer)
+    return false unless pip.is_a?(self.class)
     self.id == pip.id && self.name == pip.name && self.email == pip.email
   end
 end
 
 describe Virgola do
 
-  let(:dev)  { Developer.new { |p| p.id = 1; p.name = 'John Snow'; p.email = 'john@snow.com' } }
-  let(:task) { Task.new      { |t| t.title = 'The Title'; t.description = 'The Description'  } }
+  let(:dev)     { Developer.new { |p| p.id = 1; p.name = 'John Snow' } }
+  let(:task)    { Task.new      { |t| t.title = 'The Title'; t.description = 'The Description'  } }
+  let(:profile) { CompanyProfile.new { |c| c.email = 'john@snow.com'; c.phone = 017610101010    } }
 
   it 'should respond to column attribute methods' do
-    %w(id name email).each { |field|
+    %w(id name).each { |field|
       self.dev.should respond_to "#{field}="
       self.dev.should respond_to "#{field}"
       self.dev.should respond_to "#{field}?"
@@ -61,7 +74,7 @@ describe Virgola do
     self.dev.should respond_to "tasks="
     self.dev.should respond_to "tasks?"
     self.dev.should respond_to "tasks<<"
-    self.dev.tasks.should == []
+    self.dev.tasks.should be_empty
   end
 
   it 'should respond to has_one attribute methods' do
@@ -76,6 +89,19 @@ describe Virgola do
     self.task.should respond_to "developer?"
     self.task.should respond_to "developer="
     self.task.developer.should be_nil
+  end
+
+  it 'should allow initializing has_one relations' do
+    self.dev.profile.should be_nil
+    self.dev.profile= self.profile
+    self.dev.profile.should == self.profile
+  end
+
+  it 'should allow adding more children to has_many relation' do
+    -> {
+      self.dev.tasks << self.task
+    }.should change(self.dev.tasks, :size).from(0).to(1)
+
   end
 
   #
